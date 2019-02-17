205617e4b2e5c2f07bacf00b641dc4eab67bbf3d
rattaponk <rattapon.kaewpinjai@gmail.com>
2019-02-17 21:30:52 +0700

    finish todo detail

--- src/page/user/todo/todo.component.ts
+++ src/page/user/todo/todo.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { UserService } from '../user.service';
+import { Todo } from "../../../shared/model/todo";
+// @ViewChild('myModal') myModal;
 
 @Component({
   selector: 'app-todo',
@@ -7,9 +13,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoComponent implements OnInit {
 
-  constructor() { }
+  todos: Todo[];
+  selectedTodo: Todo;
+  styleModal: { [key: string]: string };
+
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+    this.getTodo();
+    this.styleModal = { display: 'none' };
   }
 
+  getTodo(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.userService.getTodo(id)
+      .subscribe(
+        todoapi => this.todos = todoapi,
+        err => console.error(err));
+
+  }
+
+  editTitle(todo: Todo): void {
+    // this.myModal.nativeElement.className = 'modal fade show';
+    this.styleModal = { display: 'block' };
+    console.log({ todo })
+    this.selectedTodo = todo;
+  }
+
+  saveTitle(): void {
+    console.log('Save success.');
+    console.log(this.selectedTodo)
+  }
+  closeModal(): void {
+    this.styleModal = { display: 'none' }
+  }
+  goBack(): void {
+    this.location.back();
+  }
 }
--- src/page/user/user.service.ts
+++ src/page/user/user.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Observable, of, from } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+import { Data } from '@angular/router';
+
+import { Todo } from '../../shared/model/todo'
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+
+  constructor(
+    private http: HttpClient) { }
+
+  getTodo(id: number): Observable<Todo[]> {
+    let params = new HttpParams().set('userId', id.toString());
+    const url = `https://jsonplaceholder.typicode.com/todos`;
+    return this.http.get<Todo[]>(url, { params: params });
+  }
 }
