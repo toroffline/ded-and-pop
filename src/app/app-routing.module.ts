363021100e8af0a33d87004bdb3a288fa30d051a
rattaponk <rattapon.kaewpinjai@gmail.com>
2019-02-17 18:20:31 +0700

    +add user todo route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { TodoComponent } from '../page/user/todo/todo.component'
+
+const routes: Routes = [
+  { path: 'user/:id/todo/detail', component: TodoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
